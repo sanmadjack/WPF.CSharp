3ff74d0f66127d0527e16b7bde6aaf2c99c0f228
Matthew Barbour <sanmadjack@gmail.com>
2012-08-11 00:13:40 -0500

    Added new custom window border layout

--- CustomWindowBorder.cs
+++ CustomWindowBorder.cs
@@ -4,65 +4,233 @@ using System.Linq;
 using System.Text;
 using System.Windows.Controls;
 using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 namespace SMJ.WPF {
-   public class CustomWindowBorders: UserControl {
+    enum Horiz {
+        Left, Middle, Right
+    }
+    enum Vert {
+        Top, Middle, Bottom
+    }
+
+    public class CustomWindowBorder : UserControl {
         Window parentWindow;
-        public CustomWindowBorders() {
-            parentWindow = Window.GetWindow(this);
+        public CustomWindowBorder() {
+            this.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+            this.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+            this.Visibility = System.Windows.Visibility.Visible;
+            _btmItems.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
+        }
+        Rectangle Background = new Rectangle();
+        Rectangle WhitePart = new Rectangle();
+        Grid Maincontent = new Grid();
 
 
-       }
-        Border ContentBorder = new Border();
-        public new UIElement Content {
+        public UIElementCollection BorderContent {
             get {
-                return ContentBorder.Child = Content;
+                return Maincontent.Children;
             }
             set {
-                ContentBorder.Child = Content;
+                foreach (UIElement ele in value) {
+                    Maincontent.Children.Add(ele);
+                }
             }
         }
 
+        private Grid _btmItems = new Grid();
+        public UIElementCollection BottomGrid {
+            get {
+                return _btmItems.Children;
+            }
+            set {
+                foreach (UIElement ele in value) {
+                    _btmItems.Children.Add(ele);
+                }
+            }
+        }
+        protected override void OnInitialized(EventArgs e) {
+            base.OnInitialized(e);
+            parentWindow = Window.GetWindow(this);
+            if (parentWindow != null) {
+                parentWindow.WindowStyle = WindowStyle.None;
+                parentWindow.AllowsTransparency = true;
+                parentWindow.Background = new SolidColorBrush(Colors.Transparent);
+                if(parentWindow.ResizeMode== ResizeMode.CanResize)
+                    parentWindow.ResizeMode = ResizeMode.CanResizeWithGrip;
+                hwndSource = System.Windows.PresentationSource.FromVisual(parentWindow) as System.Windows.Interop.HwndSource;
+                parentWindow.MouseLeftButtonDown += new System.Windows.Input.MouseButtonEventHandler(superGrid_MouseLeftButtonDown);
+            }
 
-       protected override void OnInitialized(EventArgs e) {
-           base.OnInitialized(e);
-           Grid superGrid = new Grid();
-           superGrid.MouseLeftButtonDown += new System.Windows.Input.MouseButtonEventHandler(superGrid_MouseLeftButtonDown);
-           ColumnDefinition column;
+            Color black = Color.FromArgb(255, 0, 0, 0);
+            Color semi = Color.FromArgb(200, 0, 0, 0);
+            GradientStopCollection stops = new GradientStopCollection();
+            stops.Add(new GradientStop(semi, 0));
+            stops.Add(new GradientStop(black, 0.5));
+            stops.Add(new GradientStop(semi, 1));
+            Background.Fill = new LinearGradientBrush(stops, 90.0);
+            Background.RadiusX = 3;
+            Background.RadiusY = 3;
 
-           column = new ColumnDefinition();
-           column.Width = new GridLength(1);
-           superGrid.ColumnDefinitions.Add(column);
 
-           column = new ColumnDefinition();
-           column.Width = new GridLength(1, GridUnitType.Star);
-           superGrid.ColumnDefinitions.Add(column);
+            stops = new GradientStopCollection();
+            stops.Add(new GradientStop(Colors.White, 0));
+            stops.Add(new GradientStop(Colors.WhiteSmoke, 0.5));
+            stops.Add(new GradientStop(Colors.White, 1));
 
-           column = new ColumnDefinition();
-           column.Width = new GridLength(1);
-           superGrid.ColumnDefinitions.Add(column);
+            WhitePart.Fill = new LinearGradientBrush(stops, 90.0);
+            WhitePart.RadiusX = 3;
+            WhitePart.RadiusY = 3;
 
-           RowDefinition row;
 
-           row = new RowDefinition();
-           row.Height = new GridLength(20);
-           superGrid.RowDefinitions.Add(row);
+            Grid superGrid = new Grid();
+            superGrid.ClipToBounds = true;
+            superGrid.Children.Add(Background);
 
-           row = new RowDefinition();
-           row.Height = new GridLength(1, GridUnitType.Star);
-           superGrid.RowDefinitions.Add(row);
+            Grid.SetColumn(WhitePart, 1);
+            Grid.SetRow(WhitePart, 1);
+            superGrid.Children.Add(WhitePart);
 
-           row = new RowDefinition();
-           row.Height = new GridLength(5);
-           superGrid.RowDefinitions.Add(row);
 
-           superGrid.Children.Add(ContentBorder);
+            superGrid.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+            superGrid.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+            ColumnDefinition column;
 
-           this.AddChild(superGrid);
+            column = new ColumnDefinition();
+            column.Width = new GridLength(1);
+            superGrid.ColumnDefinitions.Add(column);
+
+            column = new ColumnDefinition();
+            column.Width = new GridLength(1, GridUnitType.Star);
+            superGrid.ColumnDefinitions.Add(column);
+
+            column = new ColumnDefinition();
+            column.Width = new GridLength(1);
+            superGrid.ColumnDefinitions.Add(column);
+
+            RowDefinition row;
+
+            row = new RowDefinition();
+            row.Height = new GridLength(5);
+            row.MinHeight = 5;
+            superGrid.RowDefinitions.Add(row);
+
+            row = new RowDefinition();
+            row.Height = new GridLength(1, GridUnitType.Star);
+            superGrid.RowDefinitions.Add(row);
+
+            row = new RowDefinition();
+            row.Height = new GridLength(100, GridUnitType.Auto);
+            row.MinHeight = 5;
+            superGrid.RowDefinitions.Add(row);
+
+
+            Queue<Cursor> cursors = new Queue<Cursor>();
+            cursors.Enqueue(Cursors.SizeNWSE);
+            cursors.Enqueue(Cursors.SizeNS);
+            cursors.Enqueue(Cursors.SizeNESW);
+            cursors.Enqueue(Cursors.SizeWE);
+            cursors.Enqueue(Cursors.SizeWE);
+            cursors.Enqueue(Cursors.SizeNESW);
+            cursors.Enqueue(Cursors.SizeNS);
+            cursors.Enqueue(Cursors.SizeNWSE);
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (i == 1 && j == 1)
+                        continue;
+                    Rectangle rect = new Rectangle();
+                    rect.Fill = new SolidColorBrush(Colors.Red);
+                    rect.Opacity = 0;
+                    rect.MouseLeftButtonDown += new MouseButtonEventHandler(rect_MouseLeftButtonDown);
+
+                    rect.Cursor = cursors.Dequeue();
+                    Canvas.SetZIndex(rect, 99);
+                    Grid.SetColumn(rect, j);
+                    Grid.SetRow(rect, i);
+
+                    //superGrid.Children.Add(rect);
+                }
+            }
+
+            Grid.SetColumn(Background, 0);
+            Grid.SetColumnSpan(Background, 3);
+            Grid.SetRow(Background, 0);
+            Grid.SetRowSpan(Background, 6);
+
+            Grid.SetRow(_btmItems, 2);
+            Grid.SetColumn(_btmItems, 1);
+            superGrid.Children.Add(_btmItems);
+
+            if(parentWindow!=null&&parentWindow.ResizeMode == ResizeMode.CanResizeWithGrip)
+                _btmItems.Margin = new Thickness(0, 0, 15, 0);
+
+            Grid.SetColumn(Maincontent, 1);
+            Grid.SetRow(Maincontent, 1);
+            superGrid.Children.Add(Maincontent);
+
+            base.Content = superGrid;
+
+
         }
 
-       void superGrid_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
-           parentWindow.DragMove();
-       }
+        void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            int r = Grid.GetRow(sender as UIElement);
+            int c = Grid.GetColumn(sender as UIElement);
+            int num = 0;
+            switch ((Horiz)c) {
+                case Horiz.Left:
+                    num += 1;
+                    break;
+                case Horiz.Right:
+                    num += 2;
+                    break;
+            }
+            switch ((Vert)r) {
+                case Vert.Top:
+                    num += 3;
+                    break;
+                case Vert.Bottom:
+                    num += 8;
+                    break;
+            }
+
+            if (num == 0)
+                return;
+
+//            ResizeWindow((ResizeDirection)num);
+        }
+
+        private void InitializeWindowSource(object sender, EventArgs e) {
+        }
+
+        public enum ResizeDirection {
+            Left = 1,
+            Right = 2,
+            Top = 3,
+            TopLeft = 4,
+            TopRight = 5,
+            Bottom = 6,
+            BottomLeft = 7,
+            BottomRight = 8,
+        }
+        private const int WM_SYSCOMMAND = 274;
+        private HwndSource hwndSource;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        private void ResizeWindow(ResizeDirection direction) {
+            SendMessage(hwndSource.Handle, WM_SYSCOMMAND, (IntPtr)(61440 + direction), IntPtr.Zero);
+        }
+
+        void superGrid_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            parentWindow.DragMove();
+        }
 
     }
 }
