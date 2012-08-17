90b58c95a7d47787cbb116f673c07813f2bbe015
Matthew Barbour <sanmadjack@gmail.com>
2012-08-17 08:23:14 -0500

    Added auto-incrementing build number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows.Markup;
 [assembly: AssemblyTitle("WPF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Matthew Barboru")]
+[assembly: AssemblyCompany("Matthew Barbour")]
 [assembly: AssemblyProduct("WPF")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -32,7 +32,7 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: XmlnsDefinition("https://github.com/sanmadjack/WPF", "SMJ.WPF")]
